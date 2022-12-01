c6ca5f18c251601ee1c02a2733d21e08e59b091f
Sachin Ranchod <sachin.ranchod@gmail.com>
2022-12-01 14:05:27 -0800

    add missing files

---
+++ src/utils/writeClientSamples.ts
@@ -0,0 +1,51 @@
+import { join, resolve } from 'path';
+
+import type { Service } from '../client/interfaces/Service';
+import type { HttpClient } from '../HttpClient';
+import type { Indent } from '../Indent';
+import { mkdir, writeFile } from './fileSystem';
+import { formatCode as f } from './formatCode';
+import { formatIndentation as i } from './formatIndentation';
+import { isDefined } from './isDefined';
+import type { Templates } from './registerHandlebarTemplates';
+
+/**
+ * Generate Services using the Handlebar template and write to disk.
+ * @param services Array of Services to write
+ * @param templates The loaded handlebar templates
+ * @param outputPath Directory to write the generated files to
+ * @param httpClient The selected httpClient (fetch, xhr, node or axios)
+ * @param useUnionTypes Use union types instead of enums
+ * @param useOptions Use options or arguments functions
+ * @param indent Indentation options (4, 2 or tab)
+ * @param postfix Service name postfix
+ * @param clientName Custom client class name
+ */
+export const writeClientSamples = async (
+    services: Service[],
+    templates: Templates,
+    outputPath: string,
+    httpClient: HttpClient,
+    useUnionTypes: boolean,
+    useOptions: boolean,
+    indent: Indent,
+    postfix: string,
+    clientName?: string
+): Promise<void> => {
+    for (const service of services) {
+        await mkdir(join(outputPath, service.name));
+        for (const operation of service.operations) {
+            const file = resolve(outputPath, service.name, `${operation.name}${postfix}.ts`);
+            const templateResult = templates.exports.sample({
+                ...operation,
+                serviceName: service.name,
+                httpClient,
+                useUnionTypes,
+                useOptions,
+                postfix,
+                exportClient: isDefined(clientName),
+            });
+            await writeFile(file, i(f(templateResult), indent));
+        }
+    }
+};
